95c881a25e9156357eb949091a18a3e247c20772
senthilalagesan <62317332+senthilalagesan@users.noreply.github.com>
2020-05-21 01:18:23 -0500

    Add files via upload

---
+++ CalculationService/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/CalculationService/Tests/CalculationService.bwt#/"/>
+  <EmulationDataFile>ACED000574001643616C63756C6174696F6E536572766963652E627774</EmulationDataFile>
+</emulation:BWTFile>
